a7154d86ddba07d5ddaa803066f76f7c552d03d5
JYaromich <jane19937788@gmail.com>
2024-01-16 19:50:01 +0300

    init: hibernate configuration added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try(SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        };
     }
 }
